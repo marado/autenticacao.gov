jorgedsmatias/eidmw

sign-pkcs7.cpp: Added support to build against OpenSSL 1.1 except for add_signingCertificate() which has code temporarly disabled to allow the build process to complete. PKCS7 Signing doesn't work at the moment.

applayer/sign-pkcs7.cpp
@@ -111,28 +111,48 @@ void add_signed_time(PKCS7_SIGNER_INFO *si)
 unsigned int SHA256_Wrapper(unsigned char *data, unsigned long data_len, unsigned char *digest)
 {
 
-	EVP_MD_CTX cmd_ctx;
-	unsigned int md_len = 0;
+        unsigned int md_len = 0;
 
-	//Calculate the hash from the data
-	EVP_DigestInit(&cmd_ctx, EVP_sha256());
-	EVP_DigestUpdate(&cmd_ctx, data, data_len);
-    EVP_DigestFinal(&cmd_ctx, digest, &md_len);
+#if OPENSSL_VERSION_NUMBER < 0x10100000L
+        EVP_MD_CTX cmd_ctx;
 
-	return md_len;
+        //Calculate the hash from the data
+        EVP_DigestInit(&cmd_ctx, EVP_sha256());
+        EVP_DigestUpdate(&cmd_ctx, data, data_len);
+        EVP_DigestFinal(&cmd_ctx, digest, &md_len);
+#else
+        EVP_MD_CTX *cmd_ctx = EVP_MD_CTX_new();
+        //Calculate the hash from the data
+        EVP_DigestInit(cmd_ctx, EVP_sha256());
+        EVP_DigestUpdate(cmd_ctx, data, data_len);
+        EVP_DigestFinal(cmd_ctx, digest, &md_len);
+        EVP_MD_CTX_free(cmd_ctx);
+#endif
+
+        return md_len;
 
 }
 
 unsigned int SHA1_Wrapper(unsigned char *data, unsigned long data_len, unsigned char *digest)
 {
 
-	EVP_MD_CTX cmd_ctx;
 	unsigned int md_len = 0;
+
+#if OPENSSL_VERSION_NUMBER < 0x10100000L
+	EVP_MD_CTX cmd_ctx;
 
 	//Calculate the hash from the data
 	EVP_DigestInit(&cmd_ctx, EVP_sha1());
 	EVP_DigestUpdate(&cmd_ctx, data, data_len);
-    EVP_DigestFinal(&cmd_ctx, digest, &md_len);
+        EVP_DigestFinal(&cmd_ctx, digest, &md_len);
+#else
+        EVP_MD_CTX *cmd_ctx = EVP_MD_CTX_new();
+        //Calculate the hash from the data
+        EVP_DigestInit(cmd_ctx, EVP_sha1());
+        EVP_DigestUpdate(cmd_ctx, data, data_len);
+        EVP_DigestFinal(cmd_ctx, digest, &md_len);
+        EVP_MD_CTX_free(cmd_ctx);
+#endif
 
 	return md_len;
 
@@ -194,7 +214,11 @@ int append_tsp_token(PKCS7_SIGNER_INFO *sinfo, unsigned char *token, int token_l
 #endif
 
     TS_VERIFY_CTX * verify_ctx = TS_VERIFY_CTX_new();
+#if OPENSSL_VERSION_NUMBER < 0x10100000L
     verify_ctx->flags = TS_VFY_VERSION;
+#else
+    TS_VERIFY_CTX_add_flags(verify_ctx, TS_VFY_VERSION);
+#endif
 
     if(TS_RESP_verify_response(verify_ctx, tsp) != 1) {
 
@@ -205,7 +229,13 @@ int append_tsp_token(PKCS7_SIGNER_INFO *sinfo, unsigned char *token, int token_l
 	if (tsp != NULL)
 	{
 
+
+#if OPENSSL_VERSION_NUMBER < 0x10100000L
 		PKCS7* token = tsp->token;
+#else
+		PKCS7* token = TS_RESP_get_token(tsp);
+
+#endif
 
 		int p7_len = i2d_PKCS7(token, NULL);
 		unsigned char *p7_der = (unsigned char *)OPENSSL_malloc(p7_len);
@@ -247,6 +277,7 @@ void add_signingCertificate(PKCS7_SIGNER_INFO *si, X509 *x509, unsigned char * c
 	int signed_string_nid = -1;
 	ESS_SIGNING_CERT *sc = NULL;
 	ESS_CERT_ID *cid;
+	
 
 	GENERAL_NAME * name = NULL;
 
@@ -258,29 +289,33 @@ void add_signingCertificate(PKCS7_SIGNER_INFO *si, X509 *x509, unsigned char * c
 		goto end;
 
 	/* Adding the signing certificate id. */
-	if (!(cid = ESS_CERT_ID_new()))
-		goto end;
-	if (!ASN1_OCTET_STRING_set(cid->hash, cert_sha256_sum,
-		sizeof(cert_sha256_sum)))
-		goto end;
-
-	//Add Issuer and Serial Number
-
-	if (!(cid->issuer_serial = ESS_ISSUER_SERIAL_new()))
-		goto end;
+//	if (!(cid = ESS_CERT_ID_new()))
+//		goto end;
+// (temporarly disabled for the build to complete)	if (!ASN1_OCTET_STRING_set(cid->hash, cert_sha256_sum,
+//		sizeof(cert_sha256_sum)))
+//		goto end;
+//
+//	//Add Issuer and Serial Number
+//
+//	if (!(cid->issuer_serial = ESS_ISSUER_SERIAL_new()))
+//		goto end;
                 /* Creating general name from the certificate issuer. */
 	if (!(name = GENERAL_NAME_new()))
 		goto end;
 		name->type = GEN_DIRNAME;
+#if OPENSSL_VERSION_NUMBER < 0x10100000L
 	if (!(name->d.dirn = X509_NAME_dup(x509->cert_info->issuer)))
+#else
+	if (!(name->d.dirn = X509_NAME_dup(X509_get_issuer_name(x509))))
+#endif
 		goto end;
-	if (!sk_GENERAL_NAME_push(cid->issuer_serial->issuer, name))
-		goto end;
-
-	cid->issuer_serial->serial = X509_get_serialNumber(x509);
-
-	if (!sk_ESS_CERT_ID_push(sc->cert_ids, cid))
-		goto end;
+//(temporarly disabled for the build to complete) 	if (!sk_GENERAL_NAME_push(cid->issuer_serial->issuer, name))
+//		goto end;
+//
+//	cid->issuer_serial->serial = X509_get_serialNumber(x509);
+//
+//	if (!sk_ESS_CERT_ID_push(sc->cert_ids, cid))
+//		goto end;
 
 	/* Add SigningCertificateV2 signed attribute to the signer info. */
 

marado/autenticacao.gov

pteid-mw-pt/_src/eidmw/applayer/sign-pkcs7.cpp
@@ -122,28 +122,48 @@ void add_signed_time(PKCS7_SIGNER_INFO *si)
 unsigned int SHA256_Wrapper(unsigned char *data, unsigned long data_len, unsigned char *digest)
 {
 
-	EVP_MD_CTX cmd_ctx;
-	unsigned int md_len = 0;
+        unsigned int md_len = 0;
 
-	//Calculate the hash from the data
-	EVP_DigestInit(&cmd_ctx, EVP_sha256());
-	EVP_DigestUpdate(&cmd_ctx, data, data_len);
-    EVP_DigestFinal(&cmd_ctx, digest, &md_len);
+#if OPENSSL_VERSION_NUMBER < 0x10100000L
+        EVP_MD_CTX cmd_ctx;
 
-	return md_len;
+        //Calculate the hash from the data
+        EVP_DigestInit(&cmd_ctx, EVP_sha256());
+        EVP_DigestUpdate(&cmd_ctx, data, data_len);
+        EVP_DigestFinal(&cmd_ctx, digest, &md_len);
+#else
+        EVP_MD_CTX *cmd_ctx = EVP_MD_CTX_new();
+        //Calculate the hash from the data
+        EVP_DigestInit(cmd_ctx, EVP_sha256());
+        EVP_DigestUpdate(cmd_ctx, data, data_len);
+        EVP_DigestFinal(cmd_ctx, digest, &md_len);
+        EVP_MD_CTX_free(cmd_ctx);
+#endif
+
+        return md_len;
 
 }
 
 unsigned int SHA1_Wrapper(unsigned char *data, unsigned long data_len, unsigned char *digest)
 {
 
-	EVP_MD_CTX cmd_ctx;
 	unsigned int md_len = 0;
+
+#if OPENSSL_VERSION_NUMBER < 0x10100000L
+	EVP_MD_CTX cmd_ctx;
 
 	//Calculate the hash from the data
 	EVP_DigestInit(&cmd_ctx, EVP_sha1());
 	EVP_DigestUpdate(&cmd_ctx, data, data_len);
-    EVP_DigestFinal(&cmd_ctx, digest, &md_len);
+        EVP_DigestFinal(&cmd_ctx, digest, &md_len);
+#else
+        EVP_MD_CTX *cmd_ctx = EVP_MD_CTX_new();
+        //Calculate the hash from the data
+        EVP_DigestInit(cmd_ctx, EVP_sha1());
+        EVP_DigestUpdate(cmd_ctx, data, data_len);
+        EVP_DigestFinal(cmd_ctx, digest, &md_len);
+        EVP_MD_CTX_free(cmd_ctx);
+#endif
 
 	return md_len;
 
@@ -207,7 +227,11 @@ int append_tsp_token(PKCS7_SIGNER_INFO *sinfo, unsigned char *token, int token_l
 	if (tsresp != NULL)
 	{
 		TS_VERIFY_CTX * verify_ctx = TS_VERIFY_CTX_new();
+#if OPENSSL_VERSION_NUMBER < 0x10100000L
 		verify_ctx->flags = TS_VFY_VERSION;
+#else
+		TS_VERIFY_CTX_add_flags(verify_ctx, TS_VFY_VERSION);
+#endif
 
 		if (TS_RESP_verify_response(verify_ctx, tsresp) != 1) {
 
@@ -220,7 +244,11 @@ int append_tsp_token(PKCS7_SIGNER_INFO *sinfo, unsigned char *token, int token_l
 
 		TS_VERIFY_CTX_free(verify_ctx);
 
+#if OPENSSL_VERSION_NUMBER < 0x10100000L
 		PKCS7* token = tsresp->token;
+#else
+		PKCS7* token = TS_RESP_get_token(tsresp);
+#endif
 
 		int p7_len = i2d_PKCS7(token, NULL);
 		unsigned char *p7_der = (unsigned char *)OPENSSL_malloc(p7_len);
@@ -264,6 +292,7 @@ void add_signingCertificate(PKCS7_SIGNER_INFO *si, X509 *x509, unsigned char * c
 	int signed_string_nid = -1;
 	ESS_SIGNING_CERT *sc = NULL;
 	ESS_CERT_ID *cid;
+	
 
 	GENERAL_NAME * name = NULL;
 
@@ -275,29 +304,33 @@ void add_signingCertificate(PKCS7_SIGNER_INFO *si, X509 *x509, unsigned char * c
 		goto end;
 
 	/* Adding the signing certificate id. */
-	if (!(cid = ESS_CERT_ID_new()))
-		goto end;
-	if (!ASN1_OCTET_STRING_set(cid->hash, cert_sha256_sum,
-		sizeof(cert_sha256_sum)))
-		goto end;
-
-	//Add Issuer and Serial Number
-
-	if (!(cid->issuer_serial = ESS_ISSUER_SERIAL_new()))
-		goto end;
+//	if (!(cid = ESS_CERT_ID_new()))
+//		goto end;
+// (temporarly disabled for the build to complete)	if (!ASN1_OCTET_STRING_set(cid->hash, cert_sha256_sum,
+//		sizeof(cert_sha256_sum)))
+//		goto end;
+//
+//	//Add Issuer and Serial Number
+//
+//	if (!(cid->issuer_serial = ESS_ISSUER_SERIAL_new()))
+//		goto end;
                 /* Creating general name from the certificate issuer. */
 	if (!(name = GENERAL_NAME_new()))
 		goto end;
 		name->type = GEN_DIRNAME;
+#if OPENSSL_VERSION_NUMBER < 0x10100000L
 	if (!(name->d.dirn = X509_NAME_dup(x509->cert_info->issuer)))
+#else
+	if (!(name->d.dirn = X509_NAME_dup(X509_get_issuer_name(x509))))
+#endif
 		goto end;
-	if (!sk_GENERAL_NAME_push(cid->issuer_serial->issuer, name))
-		goto end;
-
-	cid->issuer_serial->serial = X509_get_serialNumber(x509);
-
-	if (!sk_ESS_CERT_ID_push(sc->cert_ids, cid))
-		goto end;
+//(temporarly disabled for the build to complete) 	if (!sk_GENERAL_NAME_push(cid->issuer_serial->issuer, name))
+//		goto end;
+//
+//	cid->issuer_serial->serial = X509_get_serialNumber(x509);
+//
+//	if (!sk_ESS_CERT_ID_push(sc->cert_ids, cid))
+//		goto end;
 
 	/* Add SigningCertificateV2 signed attribute to the signer info. */
 